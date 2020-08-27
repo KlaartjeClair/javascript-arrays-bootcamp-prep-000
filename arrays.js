f7ad5bf4ed0cfb9f4cdd07a590596bc0269fc464
Klaartje Claire <rosemarieclaire@gmail.com>
2020-08-27 15:47:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,42 @@
 var chocolateBars = [
-  "snickers",
-  "hundred grand",
-  "kitcat",
-  "skittles"];
+"snickers",
+"hundred grand",
+"kitkat",
+"skittles"
+];
 
 function addElementToBeginningOfArray(){
-chocolateBars.unshift("foo");
-...chocolateBars, "foo";
+[...chocolateBars, "foo"];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(){
+  chocolateBars.unshift("foo");
+}  
+
+function addElementToEndOfArray(){
+  ["foo", ...chocolateBars];
 } 
-  
-  
+
+function destructivelyAddElementToEndOfArray(){
+  chocolateBars.push("foo");
+}
+
+var anArray = [];
+function accessElementInArray(){
+ }
+ 
+function destructivelyRemoveElementFromBeginningOfArray(){
+ anArray.slice(1);
+}
+
+function removeElementFromBeginningOfArray(){
+ anArray.slice(0);
+}
+
+function desrtuctiveliyRemoveElemntFromEndOfArray(){
+ anArray.pop();  
+}
+
+function removeElementFromEndOfArray(){
+ anArray.slice(-1);
+  }
--- test/arrays-test.js
+++ test/arrays-test.js
@@ -8,13 +8,13 @@ const path = require('path')
 describe('arrays', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
-  })
+  });
 
   describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
-    })
-  })
+      expect(chocolateBars).to.eql('snickers','hundred grand','kitkat', 'skittles');
+    });
+  });
 
   describe('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
